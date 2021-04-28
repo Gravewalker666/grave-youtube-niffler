4484e8c464484be515a1cb2bb1db2fb759121d3c
Gravewalker666 <heshanandrews99@gmail.com>
2021-04-28 10:33:57 +0530

    Initialise the spring project

---
+++ src/main/java/com/gravewalker/graveyoutubeniffler/GraveYoutubeNifflerApplication.java
@@ -0,0 +1,13 @@
+package com.gravewalker.graveyoutubeniffler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraveYoutubeNifflerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraveYoutubeNifflerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gravewalker/graveyoutubeniffler/GraveYoutubeNifflerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gravewalker.graveyoutubeniffler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraveYoutubeNifflerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
