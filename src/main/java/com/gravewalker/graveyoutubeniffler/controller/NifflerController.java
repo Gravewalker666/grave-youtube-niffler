a7fc392d03564975ffee9620b2ad8d9dd76756cd
Gravewalker666 <heshanandrews99@gmail.com>
2021-04-29 12:01:31 +0530

    Create endpooint to get videos with statistics

--- src/main/java/com/gravewalker/graveyoutubeniffler/controller/NifflerController.java
+++ src/main/java/com/gravewalker/graveyoutubeniffler/controller/NifflerController.java
@@ -17,10 +17,11 @@ import java.nio.charset.StandardCharsets;
 @RestController
 public class NifflerController {
 
-    @GetMapping("/videoSnippets")
+    static String api_key = System.getenv().get("API_KEY");
+
+    @GetMapping("/playlist")
     @ResponseStatus(HttpStatus.OK)
-    public void getVideos(@RequestParam String playlistId, HttpServletResponse response) throws IOException {
-        String api_key = System.getenv().get("API_KEY");
+    public void getPlaylist(@RequestParam String playlistId, HttpServletResponse response) throws IOException {
         String url = "https://www.googleapis.com/youtube/v3/playlistItems?" +
                 "part=snippet&" +
                 "playlistId=" + playlistId +
@@ -28,6 +29,33 @@ public class NifflerController {
 
         HttpClient httpClient = HttpClients.createDefault();
         HttpGet executor = new HttpGet(url);
+        try {
+            HttpResponse httpResponse = httpClient.execute(executor);
+            BufferedReader br = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent(),
+                    StandardCharsets.UTF_8));
+            StringBuilder processedResponse = new StringBuilder();
+            String row;
+            while ((row = br.readLine()) != null) {
+                processedResponse.append(row);
+            }
+            br.close();
+            response.setContentType("application/json");
+            response.getWriter().write(processedResponse.toString());
+        } catch (IOException e) {
+            response.setStatus(500);
+        }
+    }
+
+    @GetMapping("/videos")
+    @ResponseStatus(HttpStatus.OK)
+    public void getVideos(@RequestParam String[] videoIds, HttpServletResponse response) throws IOException {
+        StringBuilder url = new StringBuilder("https://www.googleapis.com/youtube/v3/videos?part=snippet&part=statistics");
+        for (String id: videoIds) {
+            url.append("&id=").append(id);
+        }
+        url.append("&key=").append(api_key);
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet executor = new HttpGet(url.toString());
         try {
             HttpResponse httpResponse = httpClient.execute(executor);
             BufferedReader br = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent(),
